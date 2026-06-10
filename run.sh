3aecd47d0efde3e2bca8c1afc5821584d9019c0c
Dhruv Kumarjiguda <dhruv.k@myelinfoundry.com>
2026-06-10 12:03:43 +0530

    updated shell script for test.py

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo "Processing Vid2"
 echo "================================="
 
 uv run test.py \
-  --video ./data/Vid2.mp4 \
-  --camera-name Vid2 \
+  --video ./data/D03_deliside_cashbelowphone_001_720.mp4 \
+  --camera-name D03 \
   --roi "566,408;745,410;740,175;558,165;558,399"
 
 echo
@@ -17,8 +17,8 @@ echo "Processing Vid3"
 echo "================================="
 
 uv run test.py \
-  --video ./data/Vid3.mp4 \
-  --camera-name Vid3 \
+  --video ./data/D03_deliside_cashinpocket_002_720.mp4 \
+  --camera-name D03 \
   --roi "734,396;724,207;476,199;468,257;621,272;625,399;735,396"
 
 echo
@@ -27,8 +27,8 @@ echo "Processing Vid4"
 echo "================================="
 
 uv run test.py \
-  --video ./data/Vid4.mp4 \
-  --camera-name Vid4 \
+  --video ./data/D02_glassside_cashbelowphone_002_720.mp4 \
+  --camera-name D02 \
   --roi "648,481;858,404;721,223;546,292;647,475"
 
 echo
